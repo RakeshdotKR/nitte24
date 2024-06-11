6cfc70872351f4de51811a052b904a230a0cb99a
RakeshdotKR <70848422+RakeshdotKR@users.noreply.github.com>
2024-06-11 16:24:49 +0530

    Create 10.cpp

---
+++ Day4/10.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL || head->next == NULL || head->next->next==NULL){
+            return head;
+        }
+        ListNode* oddtail = head;
+        ListNode* oddhead = head;
+        ListNode* eventail = head->next;
+        ListNode* evenhead = head->next;
+
+        while(eventail!=NULL && eventail->next!=NULL){
+            oddtail->next = eventail->next;
+            oddtail = eventail->next;
+
+            eventail->next = oddtail->next;
+            eventail = oddtail->next;
+        }
+        oddtail->next = evenhead;
+        return oddhead;
+    }
+};
